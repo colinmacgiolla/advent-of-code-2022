23a2eb68eafebd10f72bdc20823ce61a1bd06cd0
Colin MacGiollaEain <colin@arista.com>
2022-12-06 14:01:24 +0000

    Day 6 completed

--- day-6/main.py
+++ day-6/main.py
@@ -1,10 +1,30 @@
 #!/bin/python
 
+
+
+def msg_parser(input, window=4):
+    index = 0
+
+    while (index + window) < len(input):
+
+        block = input[index:window+index]
+        if len( set(block)) != window:
+            index += 1
+        else:
+            break    
+
+    return int(window + index)
+
+
 def main():
 
-    with open('C:\\Users\\colinmac\\Documents\\Git Projects\\advent-of-code-2022\\day-6\\input\\data.txt') as f:
-        raw_input = f.read()    
-    
+    with open('C:\\Users\\Colin MacGiollaEain\\Documents\\Projects\\advent-of-code-2022\\day-6\\input\\data.txt') as f:
+        raw_input = f.read()
+
+    print("Part 1: Marker located at: %d" % msg_parser(raw_input))
+    print("Part 2: Marker located at: %d" % msg_parser(raw_input, window=14))
+
+       
     return 0
 
 
