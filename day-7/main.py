df25ba2aa00de6e722da6b7811eca9be40e487dd
Colin MacGiollaEain <colin@arista.com>
2022-12-06 16:14:09 +0000

    Ready for day 7

---
+++ day-7/main.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+
+def main():
+
+    with open('C:\\Users\\colinmac\\Documents\\Projects\\advent-of-code-2022\\day-7\\input\\data.txt') as f:
+        raw_input = f.read()
+
+
+    return 0
+
+if __name__ == "__main__":
+    main()
