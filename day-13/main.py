589439472ff245931fa72b0b194dff699d92f5b3
Colin MacGiollaEain <colin@arista.com>
2022-12-13 18:21:42 +0000

    Part 2

--- day-13/main.py
+++ day-13/main.py
@@ -2,6 +2,8 @@
 
 
 from ast import literal_eval
+from functools import cmp_to_key
+from pprint import pprint
 
 def compare_packet(entryA, entryB):
     try:
@@ -18,7 +20,7 @@ def compare_packet(entryA, entryB):
                     return False
                 else:
                     continue
-                
+
             elif isinstance(x, list) and isinstance(y, int):
                 # x is list and y is int
                 result = compare_packet(x,[y])
@@ -26,7 +28,7 @@ def compare_packet(entryA, entryB):
                     continue
                 else:
                     return result
-                
+
             elif isinstance(x, int) and isinstance(y, list):
                 # x is int and y is list
                 result = compare_packet([x], y)
@@ -36,14 +38,12 @@ def compare_packet(entryA, entryB):
                     return result
             else:
                 # Both are lists
-                result =  compare_packet(x,y)
+                result = compare_packet(x,y)
                 if result is None:
                     continue
                 else:
                     return result
-                
 
-        
         # X should be shorter then Y and we haven't hit an exit yet
         if len(entryA) < len(entryB):
             return True
@@ -51,36 +51,62 @@ def compare_packet(entryA, entryB):
         return None
     except IndexError:
         return False
-            
-    
+
+
+def insert_sort(A):
+
+    for step in range(1,len(A)):
+
+        current = A[step]
+        j = step
+
+        while j > 0 and compare_packet(current,A[j-1]):
+            A[j] = A[j-1]
+            j = j -1
+        A[j] = current
+    return A
 
 
 def main():
 
     with open('C:\\Users\\colinmac\\Documents\\Git Projects\\advent-of-code-2022\\day-13\\input\\data.txt') as f:
         raw_input = f.read()
-    
+
     entries = []
     for record in raw_input.split('\n\n'):
         entryA, entryB = record.split('\n')
         entries.append((entryA, entryB))
-        
+
     parsed_pairs = []
     for entry in entries:
         entryA = literal_eval(entry[0])
         entryB = literal_eval(entry[1])
         parsed_pairs.append((entryA, entryB))
-    
+
     correct_packets = []
     for index, pair in enumerate(parsed_pairs):
         if compare_packet(pair[0],pair[1]):
             correct_packets.append(index+1)
-    
+
     print("Part 1: The sum of the indices of the currect packets: %d" % sum(correct_packets))
-            
+
+    all_packets = [ x for y in parsed_pairs for x in y ]
+    # Add our marker packets
+    all_packets.append([[2]])
+    all_packets.append([[6]])
+
+    # Sort based on whether or not they are correct I *think*!
+
+    sorted_packets = insert_sort(all_packets)
+    #pprint(sorted_packets)
+    mark_1 = sorted_packets.index([[2]]) + 1
+    mark_2 = sorted_packets.index([[6]]) + 1
+    print("Part 2: The product of the indices of the marker packets in the sorted list is: %d" % (mark_1 * mark_2))
+
+
     print("End of Line")
     return 0
-    
+
 
 
 if __name__ == "__main__":
