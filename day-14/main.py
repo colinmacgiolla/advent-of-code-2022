b99c4ae98142fdb19f333f14484af183d2398023
Colin MacGiollaEain <colin@arista.com>
2022-12-14 17:48:11 +0000

    Part 2

--- day-14/main.py
+++ day-14/main.py
@@ -1,21 +1,7 @@
 #!/bin/python
 
-from pprint import pprint
+from copy import deepcopy
 
-    
-def render_grid(grid: dict):
-    min_X = min(grid.keys(), key=lambda x: x[0])[0]
-    min_Y = min(grid.keys(), key=lambda x: x[1])[1]
-    max_X = max(grid.keys(), key=lambda x: x[0])[0]
-    max_Y = max(grid.keys(), key=lambda x: x[1])[1]
-    
-    for y in range(min_Y,max_Y+1):
-        print("")
-        for x in range(min_X,max_X+1):
-            print("%s" % grid.get((x,y),".") , end="")
-    print("\n")
-    
-    return None
 
 
 class Grid():
@@ -27,33 +13,31 @@ class Grid():
         self.max_Y = None
         self._startX, self._startY = start_point
         self.data[start_point] = "+"
-        
+
     def add_stone( self, point ):
         self.data[ point ] = "#"
-        
+
     def add_sand(self, point):
         self.data[point] = "o"
-        
+
     def set_boundaries(self):
-        
+
         self.min_X = min(self.data.keys(), key=lambda x: x[0])[0]
         self.min_Y = min(self.data.keys(), key=lambda x: x[1])[1]
         self.max_X = max(self.data.keys(), key=lambda x: x[0])[0]
         self.max_Y = max(self.data.keys(), key=lambda x: x[1])[1]
-        
+
     def get( self, point, default="."):
         if point in self.data:
             return self.data[point]
         else:
             return default
-        
+
     def set_floor(self, floor=2):
-        
+
         for x in range(self.min_X, self.max_X+1):
             self.add_stone( (x, self.max_Y + floor) )
-        self.set_boundaries()
-        
-        
+
     def render(self):
         for y in range(self.min_Y,self.max_Y+1):
             print("")
@@ -62,33 +46,43 @@ class Grid():
         print("\n")
 
 
-def falling_sand(grid: Grid):
-    
-    sandX, sandY = grid._startX,grid._startY
-    
+def falling_sand(grid: Grid, changed=False):
 
-    while (sandX in range(grid.min_X, grid.max_X+1) and sandY in range(grid.min_Y, grid.max_Y)):
-       
+    sandX, sandY = grid._startX,grid._startY
+
+    while True:
+
+        if changed and sandY+1 == grid.max_Y:
+            grid.add_sand((sandX,sandY))
+            return True
+
+        elif not changed and sandY > grid.max_Y:
+            # stop the part 1 infinity
+            return False
+
         # current space is empty, so fall by one
-        if (sandX, sandY+1) not in grid.data:
+        elif (sandX, sandY+1) not in grid.data:
             sandY += 1
-        
+
         # there is something in the way, so try and move down
         # and to the left
         elif (sandX-1, sandY+1) not in grid.data:
             sandX -= 1
             sandY +=1
-        
+
         # otherwise try to move down and to the right
         elif (sandX+1, sandY+1) not in grid.data:
             sandX += 1
             sandY += 1
-        
+
+        elif (sandX,sandY) == (grid._startX, grid._startY):
+            grid.add_sand( (sandX, sandY) )
+            return False
         # can't move
         else:
             grid.add_sand( (sandX, sandY) )
             return True
-            
+
     return False
 
 
@@ -110,20 +104,29 @@ def main():
             for x in range( min(X_cur,X_nxt), max(X_cur,X_nxt)+1 ):
                 for y in range( min(Y_cur,Y_nxt), max(Y_cur,Y_nxt)+1 ):
                     grid.add_stone( (x,y) )
-    
+
     # Import completed
     grid.set_boundaries()
-               
-               
+    round_two = deepcopy(grid)
+
+
     counter = 0
     while falling_sand(grid):
         counter += 1
 
     grid.render()
     print("Part 1: Number of grains of sand before the abyss: %d" % counter)
-                
-         
-        
+
+    counter = 1
+    round_two.set_floor()
+    round_two.set_boundaries()
+    while falling_sand(round_two, True):
+        counter += 1
+    
+    round_two.set_boundaries()
+    round_two.render()
+    print("Part 2: Number of grains of sand before the source is blocked: %d" % counter)
+    
     
 
     print("End of Line")
